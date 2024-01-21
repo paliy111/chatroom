57b972c2e05fbe2a066e85f8758f0792a53491c1
paliy111 <pjoni1118@gmail.com>
2024-01-21 21:43:54 +0200

    added threading

--- echo_server.py
+++ echo_server.py
@@ -4,29 +4,29 @@ import logging
 import threading
 
 
-def main():
-    def handle_client(connection):
-        PRETEND_TO_DO_WORK = 5
-        time.sleep(PRETEND_TO_DO_WORK)
-        data = connection.recv(1024)
-        string = data.decode('utf-8')
-        answer = f'Echo: {string}'
-        connection.send(answer.encode('utf-8'))
-        connection.close()
+def handle_client(connection):
+    PRETEND_TO_DO_WORK = 5
+    time.sleep(PRETEND_TO_DO_WORK)
+    data = connection.recv(1024)
+    string = data.decode('utf-8')
+    answer = f'Echo: {string}'
+    connection.send(answer.encode('utf-8'))
+    connection.close()
 
-    def serve():
-        server = socket.socket()
-        server.bind(('', 3333))
-        server.listen(5)
 
-        while True:
-            connection, peer_address = server.accept()
-            logging.info(f'Connection from {peer_address}')
-            thread = threading.Thread(target=handle_client, args=(connection,))
-            thread.start()
+def serve():
+    server = socket.socket()
+    server.bind(('', 3333))
+    server.listen(5)
 
-    serve()
+    while True:
+        connection, peer_address = server.accept()
+        logging.info(f'Connection from {peer_address}')
+        thread = threading.Thread(target=handle_client, args=(connection,))
+        thread.start()
+
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
-    main()
+    serve()
