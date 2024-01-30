42c641a41226abc9e160160747fa2ef407062bf0
paliy111 <pjoni1118@gmail.com>
2024-01-30 22:10:25 +0200

    switched to using select

--- echo_server.py
+++ echo_server.py
@@ -1,32 +1,46 @@
+import select
 import socket
 import time
 import logging
-import threading
 
 
-def main():
-    def _handle_client(connection):
+def handle_reads(reads, messages):
+    for connection in reads:
+        data = connection.recv(1024)
         PRETEND_TO_DO_WORK = 5
         time.sleep(PRETEND_TO_DO_WORK)
-        data = connection.recv(1024)
         string = data.decode('utf-8')
-        answer = f'Echo: {string}'
-        connection.send(answer.encode('utf-8'))
-        connection.close()
+        message = f'Echo: {string}'
+        messages[connection] = message
 
-    def serve():
-        server = socket.socket()
-        server.bind(('', 3333))
-        server.listen(5)
 
-        while True:
+def handle_writes(writes, messages):
+    for connection in writes:
+        if connection not in messages:
+            continue
+        message = messages[connection]
+        connection.send(message.encode('utf-8'))
+        del messages[connection]
+
+
+def serve():
+    server = socket.socket()
+    server.bind(('', 3333))
+    server.listen(5)
+
+    connections = []
+    messages = {}
+    while True:
+        read, write, error = select.select(connections + [server], connections, connections)
+        if server in read:
             connection, peer_address = server.accept()
-            logging.info(f'Connection from {peer_address}')
-            thread = threading.Thread(target=_handle_client, args=(connection,))
-            thread.start()
+            logging.info(f'connection from {peer_address}')
+            connections.append(connection)
+            read.remove(server)
+        handle_reads(read, messages)
+        handle_writes(write, messages)
 
-    serve()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
-    main()
+    serve()
