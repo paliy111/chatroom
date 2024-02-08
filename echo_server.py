24aefed4d96e7b5e8aafb091c37192c09704a68a
Yoav Kleinberger <haarcuba@gmail.com>
2024-02-08 15:12:27 +0200

    refactor

--- echo_server.py
+++ echo_server.py
@@ -1,34 +1,8 @@
 import socket
-import time
 import argparse
 import select
 import logging
-
-class SafeConnection:
-    def __init__(self, connection):
-        self._connection = connection
-        self._done = False
-
-    def fileno(self):
-        return self._connection.fileno()
-
-    def _safe_call(self, function, default, *args):
-        try:
-            return function(*args)
-        except (BrokenPipeError, socket.error) as e:
-            logging.error(f'Connection closed: {e}')
-            self._done = True
-            logging.info(f'done with {self}')
-            return default
-
-    def recv(self, size):
-        return self._safe_call(self._connection.recv, b'', size)
-
-    def send(self, data):
-        return self._safe_call(self._connection.send, 0, data)
-
-    def done(self):
-        return self._done
+import safe_connection
 
 def handle_reads(read_ready, messages):
     for connection in read_ready:
@@ -48,7 +22,7 @@ def handle_writes(write_ready, messages):
 
 def accept_connection(server, connections):
     connection, peer_address = server.accept()
-    connection = SafeConnection(connection)
+    connection = safe_connection.SafeConnection(connection)
     logging.info(f'Connection from {peer_address}')
     connections.append(connection)
     return connection
---
+++ safe_connection.py
@@ -0,0 +1,29 @@
+import logging
+import socket
+
+
+class SafeConnection:
+    def __init__(self, connection):
+        self._connection = connection
+        self._done = False
+
+    def fileno(self):
+        return self._connection.fileno()
+
+    def _safe_call(self, function, default, *args):
+        try:
+            return function(*args)
+        except (BrokenPipeError, socket.error) as e:
+            logging.error(f'Connection closed: {e}')
+            self._done = True
+            logging.info(f'done with {self}')
+            return default
+
+    def recv(self, size):
+        return self._safe_call(self._connection.recv, b'', size)
+
+    def send(self, data):
+        return self._safe_call(self._connection.send, 0, data)
+
+    def done(self):
+        return self._done
