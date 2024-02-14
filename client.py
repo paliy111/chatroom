f8a58f7af7ec9c8e6878b3bfd898e0e5473a677a
odyssey <eladrahat@mail.tau.ac.il>
2024-02-14 15:03:41 +0200

    implemented threading
    to send and receive messages on the same time

--- client.py
+++ client.py
@@ -2,6 +2,8 @@ import logging
 import socket
 import argparse
 import json
+import threading
+
 
 def main(host, port, nickname):
     sock = socket.socket()
@@ -11,8 +13,17 @@ def main(host, port, nickname):
     json_sock_hello_message = json.dumps(sock_hello_message)
     sock.send(json_sock_hello_message.encode())
     response = sock.recv(1024)
-    print(f'< {response.decode()}')
+    print(f'> {response.decode()}')
 
+    reader_thread = threading.Thread(target=receiver_thread, args=(sock,))
+    reader_thread.start()
+    writer_thread = threading.Thread(target=sender_thread, args=(sock,))
+    writer_thread.start()
+    writer_thread.join()
+    reader_thread.join()
+
+
+def sender_thread(sock):
     while True:
         message = input('client> ')
         if message == 'quit':
@@ -20,17 +31,28 @@ def main(host, port, nickname):
             return
         if message == 'who':
             sock_message_send = {"code": "who"}
-        else:
+        if '|' in message:
             user, text_content = message.split('|', 1)
             if user == '*':
                 sock_message_send = {"code": "outgoing_broadcast", "content": text_content}
             else:
                 sock_message_send = {"code": "send_message", "to": user, "content": text_content}
-
+        else:
+            print('Invalid message format')
+            continue
         json_sock_message_send = json.dumps(sock_message_send)
         sock.send(json_sock_message_send.encode())
-        response = sock.recv(1024)
-        print(f'server> {response.decode()}')
+
+
+def receiver_thread(sock):
+    while True:
+        try:
+            response = sock.recv(1024)
+            print(f'server> {response.decode()}')
+        except ConnectionResetError:
+            print('Connection closed by server')
+            return
+
 
 
 if __name__ == '__main__':
